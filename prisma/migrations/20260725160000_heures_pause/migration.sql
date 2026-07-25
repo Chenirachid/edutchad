7f15a79ceb7cfbec97b354815798fa566b034361
Chenirachid <ahmatchenirachid@gmail.com>
2026-07-25 22:39:45 +0200

    r

---
+++ prisma/migrations/20260725160000_heures_pause/migration.sql
@@ -0,0 +1,2 @@
+ALTER TABLE "ParametrePlateforme" ADD COLUMN "heureDebutPause" TEXT NOT NULL DEFAULT '12:00';
+ALTER TABLE "ParametrePlateforme" ADD COLUMN "heureFinPause" TEXT NOT NULL DEFAULT '13:00';
--- src/parametres/dto/update-parametres.dto.ts
+++ src/parametres/dto/update-parametres.dto.ts
@@ -1,4 +1,4 @@
-import { IsInt, IsOptional, IsString, Max, Min } from 'class-validator';
+import { IsInt, IsOptional, IsString, Matches, Max, Min } from 'class-validator';
 
 export class UpdateParametresDto {
   @IsOptional()
@@ -14,4 +14,14 @@ export class UpdateParametresDto {
   @Min(10)
   @Max(100)
   bareme?: number;
+
+  @IsOptional()
+  @IsString()
+  @Matches(/^([01]\d|2[0-3]):[0-5]\d$/, { message: 'Format attendu : HH:MM' })
+  heureDebutPause?: string;
+
+  @IsOptional()
+  @IsString()
+  @Matches(/^([01]\d|2[0-3]):[0-5]\d$/, { message: 'Format attendu : HH:MM' })
+  heureFinPause?: string;
 }
