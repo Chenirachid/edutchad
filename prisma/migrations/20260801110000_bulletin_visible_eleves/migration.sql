7185c4aa28c004a36a9a964b083f63b14db1e25a
Chenirachid <ahmatchenirachid@gmail.com>
2026-08-01 08:43:10 +0200

    r

---
+++ prisma/migrations/20260801110000_bulletin_visible_eleves/migration.sql
@@ -0,0 +1,1 @@
+ALTER TABLE "ParametrePlateforme" ADD COLUMN "bulletinVisibleEleves" BOOLEAN NOT NULL DEFAULT false;
--- src/parametres/dto/update-parametres.dto.ts
+++ src/parametres/dto/update-parametres.dto.ts
@@ -59,4 +59,7 @@ export class UpdateParametresDto {
 
   @IsOptional()
   emploiVisibleEleves?: boolean;
+
+  @IsOptional()
+  bulletinVisibleEleves?: boolean;
 }
